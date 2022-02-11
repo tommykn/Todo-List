46a89cb1a90fa1ff43717398921369266a95182b
Tommy <tbkknight1@icloud.com>
2022-02-11 15:37:07 -0600

    made the obeject construtor for the ToDo's and made a load-page.js file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+
+
+function ToDo(task, description, dueDate, priority, difficulty) {
+    this.task = task;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.difficulty = difficulty;
+}
+
+ToDo.prototype.editTask = function(newTask) {
+    this.task = newTask;
+}
+
+const clean = new ToDo('clean', 'I will be cleaning', '10/12/32', 'High', 'low');
+console.log(clean);
+
+clean.editTask('work');
+
+console.log(clean);
