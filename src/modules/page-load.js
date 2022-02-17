3d276f7bc6f281fe400b1dabbffe5dbd0016fdfa
Tommy <tbkknight1@icloud.com>
2022-02-17 11:43:57 -0600

    added the modal to the load init page function.

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -52,6 +52,76 @@ function makeInitPage() {
     sidenav.appendChild(ul);
     mainWrapper.appendChild(sidenav);
     const main = makeContainer('main-content');
+    // modal for inputing tasks
+    const modal = makeContainer('modal');
+    const modalContent = makeContainer('modal-content');
+    modal.appendChild(modalContent);
+    const taskForm = document.createElement('form');
+    taskForm.classList.add('task-form');
+    const formTitle = makeTitle('New Task', 'modal-title');
+    taskForm.appendChild(formTitle);
+    const requiredWrapper = makeContainer('required-info'); // required-wapper
+    const modalTask = makeContainer('modal-task-wrapper');
+    modalTask.classList.add('modal-text-input');
+    const taskLabel = document.createElement('label');
+    taskLabel.setAttribute('for', 'task');
+    taskLabel.textContent = 'Task:';
+    modalTask.appendChild(taskLabel);
+    const taskInput = document.createElement('input');
+    taskInput.setAttribute('id', 'task');
+    taskInput.setAttribute('name', 'task');
+    taskInput.setAttribute('type', 'text');
+    taskInput.setAttribute('required', '');
+    modalTask.appendChild(taskInput);
+    const modalDes = makeContainer('modal-des-wrapper');
+    modalDes.classList.add('modal-text-input');
+    const desLabel = document.createElement('label');
+    desLabel.setAttribute('for', 'des');
+    desLabel.textContent= 'Description:';
+    modalDes.appendChild(desLabel);
+    const desInput = document.createElement('input');
+    desInput.setAttribute('id', 'des');
+    desInput.setAttribute('name', 'des');
+    desInput.setAttribute('type', 'text');
+    desInput.setAttribute('required', '');
+    modalDes.appendChild(desInput);
+    requiredWrapper.appendChild(modalTask);
+    requiredWrapper.appendChild(modalDes);
+    taskForm.appendChild(requiredWrapper);
+    // non-required wrapper need ot do moladcontetn.apopedntaskform
+    const nonrequiredWrapepr = makeContainer('nonrequired-info');
+    const difBtn = makeBtn('dif-btn');
+    difBtn.textContent = 'Difficulty';
+    const dateBtn = makeBtn('date-btn');
+    dateBtn.textContent = 'Due Date';
+    const priorityBtn = makeBtn('pri-btn');
+    priorityBtn.textContent = 'Priority';
+    nonrequiredWrapepr.appendChild(difBtn);
+    nonrequiredWrapepr.appendChild(dateBtn);
+    nonrequiredWrapepr.appendChild(priorityBtn);
+    taskForm.appendChild(nonrequiredWrapepr);
+    // add canel btns wrapper
+    const addCancelWrapper = makeContainer('addcancelbtns-wrapper');
+    const addBtn = makeBtn('add-btn');
+    addBtn.textContent = 'Add';
+    addBtn.setAttribute('id', 'submit-btn');
+    addBtn.setAttribute('type', 'submit');
+    const cancelBtn = makeBtn('cancel-btn');
+    cancelBtn.textContent = 'Cancel';
+    addCancelWrapper.appendChild(addBtn);
+    addCancelWrapper.appendChild(cancelBtn);
+    taskForm.appendChild(addCancelWrapper);
+    modalContent.appendChild(taskForm);
+    mainWrapper.appendChild(modal);
+
+
+
+
+
+
+
+
+
     const home = makeContainer('home');
     const homeHeader = makeContainer('home-header');
     const homeHeaderText = makeTitle('Home', 'home-text');
